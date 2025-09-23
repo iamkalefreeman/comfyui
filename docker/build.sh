86b8a95c0fe4354b2552dea443f6d0bdc262e81e
iamkalefreeman <70961907+iamkalefreeman@users.noreply.github.com>
2025-09-23 19:40:15 +0700

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -10,7 +10,7 @@ working_dir=${WORKING_DIR:-"/path/to/your/comfyui/code"}
 # docker buildx build --target qwen-models "${working_dir}" -f "${working_dir}/docker/base/Dockerfile"
 # [[ "$?" -ne 0 ]] && echo "Error!" && return 11
 
-docker buildx build --target comfyui-qwen --build-arg BASE_IMAGE="ghcr.io/saladtechnologies/comfyui-api:comfy0.3.59-api1.9.2-torch2.8.0-cuda12.8-runtime" \
+docker buildx build --target comfyui-qwen --build-arg BASE_IMAGE="ghcr.io/saladtechnologies/comfyui-api:latest" \
   -t "${docker_account}/comfyui:qwen-base-latest" "${working_dir}" \
   -f "${working_dir}/docker/base/Dockerfile"
 [[ "$?" -ne 0 ]] && echo "Error!" && return 12
@@ -48,7 +48,7 @@ docker push "${docker_account}/comfyui:qwen-full-latest"
 # # docker buildx build --target kontext-models "${working_dir}" -f "${working_dir}/docker/base/Dockerfile"
 # # [[ "$?" -ne 0 ]] && echo "Error!" && return 11
 
-# docker buildx build --target comfyui-kontext --build-arg BASE_IMAGE="ghcr.io/saladtechnologies/comfyui-api:comfy0.3.59-api1.9.2-torch2.8.0-cuda12.8-runtime" \
+# docker buildx build --target comfyui-kontext --build-arg BASE_IMAGE="ghcr.io/saladtechnologies/comfyui-api:latest" \
 #   -t "${docker_account}/comfyui:kontext-base-latest" "${working_dir}" \
 #   -f "${working_dir}/docker/base/Dockerfile"
 # [[ "$?" -ne 0 ]] && echo "Error!" && return 12
