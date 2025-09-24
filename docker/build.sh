20f1b888fdc51d6ca616045a8f9d66e6b38e2b18
iamkalefreeman <70961907+iamkalefreeman@users.noreply.github.com>
2025-09-24 19:48:20 +0700

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -4,16 +4,6 @@ date_version=$(date +'%d-%m-%Y-%H-%M')
 docker_account=${DOCKER_ACCOUNT:-YOUR_DOCKER_ACCOUNT_HERE}
 working_dir=${WORKING_DIR:-"/path/to/your/comfyui/code"}
 
-# #########################
-# # BuildBox Build
-# #########################
-# docker buildx build \
-#   -t "${docker_account}/buildbox:stable" "${working_dir}" \
-#   -f "${working_dir}/docker/buildbox/Dockerfile"
-# [[ "$?" -ne 0 ]] && echo "Error!" && return 10
-# docker push "${docker_account}/buildbox:stable"
-# [[ "$?" -ne 0 ]] && echo "Error!" && return 10
-
 #########################
 # QWEN Builds
 #########################
