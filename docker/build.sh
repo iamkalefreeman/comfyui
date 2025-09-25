6c32e32bcd53920dda6c04aab3262f43e7ddeec6
iamkalefreeman <70961907+iamkalefreeman@users.noreply.github.com>
2025-09-25 12:33:27 +0700

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -44,41 +44,4 @@ docker buildx build \
 docker push "${docker_account}/comfyui:qwen-full-latest"
 [[ "$?" -ne 0 ]] && echo "Error!" && return 15
 
-
-#########################
-# Kontext-Flux Builds
-#########################
-# # docker buildx build --target kontext-models "${working_dir}" -f "${working_dir}/docker/base/Dockerfile"
-# # [[ "$?" -ne 0 ]] && echo "Error!" && return 11
-
-# docker buildx build --target comfyui-kontext --build-arg BASE_IMAGE="ghcr.io/iamkalefreeman/comfyui-api:latest" \
-#   -t "${docker_account}/comfyui:kontext-base-latest" "${working_dir}" \
-#   -f "${working_dir}/docker/base/Dockerfile"
-# [[ "$?" -ne 0 ]] && echo "Error!" && return 12
-# docker push "${docker_account}/comfyui:kontext-base-latest"
-# [[ "$?" -ne 0 ]] && echo "Error!" && return 12
-
-# docker buildx build --build-arg BASE_IMAGE="${docker_account}/comfyui:kontext-base-latest" \
-#   -t "${docker_account}/comfyui:kontext-salad-api-latest" "${working_dir}" \
-#   -f "${working_dir}/docker/salad-api/Dockerfile"
-# [[ "$?" -ne 0 ]] && echo "Error!" && return 13
-# docker push "${docker_account}/comfyui:kontext-salad-api-latest"
-# [[ "$?" -ne 0 ]] && echo "Error!" && return 13
-
-# docker buildx build --build-arg BASE_IMAGE="${docker_account}/comfyui:kontext-base-latest" \
-#   -t "${docker_account}/comfyui:kontext-runpod-${date_version}" \
-#   -t "${docker_account}/comfyui:kontext-runpod-latest" "${working_dir}" \
-#   -f "${working_dir}/docker/runpod/Dockerfile"
-# docker push "${docker_account}/comfyui:kontext-runpod-${date_version}"
-# [[ "$?" -ne 0 ]] && echo "Error!" && return 14
-# docker push "${docker_account}/comfyui:kontext-runpod-latest"
-# [[ "$?" -ne 0 ]] && echo "Error!" && return 14
-
-# docker buildx build --build-arg BASE_IMAGE="${docker_account}/comfyui:kontext-base-latest" \
-#   -t "${docker_account}/comfyui:kontext-full-latest" "${working_dir}" \
-#   -f "${working_dir}/docker/full/Dockerfile"
-# [[ "$?" -ne 0 ]] && echo "Error!" && return 15
-# docker push "${docker_account}/comfyui:kontext-full-latest"
-# [[ "$?" -ne 0 ]] && echo "Error!" && return 15
-
 return 0
