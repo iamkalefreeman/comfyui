56dbb76294e3048e09ee57f6e515d88d9114a68b
iamkalefreeman <70961907+iamkalefreeman@users.noreply.github.com>
2025-09-26 15:44:02 +0700

    Create build-qwen-models.sh

---
+++ docker/build-qwen-models.sh
@@ -0,0 +1,18 @@
+#!bin/bash -xe
+
+date_version=$(date +'%d-%m-%Y-%H-%M')
+docker_account=${DOCKER_ACCOUNT:-YOUR_DOCKER_ACCOUNT_HERE}
+working_dir=${WORKING_DIR:-"/path/to/your/comfyui/code"}
+
+#########################
+# QWEN Builds
+#########################
+
+### Build qwen-models
+docker buildx build --target qwen-models \
+  --build-arg BUILDBOX_IMAGE="${docker_account}/buildbox:stable" \
+  -t "${docker_account}/ai-models:qwen-models-latest" "${working_dir}" \
+  -f "${working_dir}/docker/ai-models/qwen-models.Dockerfile"
+[[ "$?" -ne 0 ]] && echo "Error!" && return 11
+# docker push "${docker_account}/ai-models:qwen-models-latest"
+# [[ "$?" -ne 0 ]] && echo "Error!" && return 11
