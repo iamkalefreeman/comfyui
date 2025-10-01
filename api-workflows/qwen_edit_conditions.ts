e05d859329417baffcea3d28971b5defe5c431fb
iamkalefreeman <70961907+iamkalefreeman@users.noreply.github.com>
2025-10-01 13:59:01 +0700

    Update qwen_edit_conditions.ts

--- api-workflows/qwen_edit_conditions.ts
+++ api-workflows/qwen_edit_conditions.ts
@@ -442,7 +442,7 @@ function generateWorkflow(input: InputType): ComfyPrompt {
         delimiter: "|",
         start_index: 0,
         skip_every: 0,
-        max_count: 2,
+        max_count: 4,
       },
       class_type: "TextSplitByDelimiter",
       _meta: {
