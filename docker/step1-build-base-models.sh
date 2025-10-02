a13e6eb5e8e4f64b24f9009ccd4127df766055de
iamkalefreeman <70961907+iamkalefreeman@users.noreply.github.com>
2025-10-03 00:06:37 +0700

    Create step1-build-base-models.sh

---
+++ docker/step1-build-base-models.sh
@@ -0,0 +1,14 @@
+#!bin/bash -xe
+
+date_version=$(date +'%d-%m-%Y-%H-%M')
+docker_account=${DOCKER_ACCOUNT:-YOUR_DOCKER_ACCOUNT_HERE}
+working_dir=${WORKING_DIR:-"/path/to/your/comfyui/code"}
+
+### Build comfyui-models
+docker buildx build --target comfyui-models \
+  --build-arg BUILDBOX_IMAGE="${docker_account}/buildbox:stable" \
+  -t "${docker_account}/ai-models:comfyui-models-latest" "${working_dir}" \
+  -f "${working_dir}/docker/ai-models/comfyui-models.Dockerfile"
+[[ "$?" -ne 0 ]] && echo "Error!" && return 11
+docker push "${docker_account}/ai-models:comfyui-models-latest"
+[[ "$?" -ne 0 ]] && echo "Error!" && return 11
