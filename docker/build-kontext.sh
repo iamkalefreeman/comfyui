896bf006439f14552054621a344370d06b7398e5
iamkalefreeman <70961907+iamkalefreeman@users.noreply.github.com>
2025-10-02 23:45:54 +0700

    Update build-kontext.sh

--- docker/build-kontext.sh
+++ docker/build-kontext.sh
@@ -4,19 +4,6 @@ date_version=$(date +'%d-%m-%Y-%H-%M')
 docker_account=${DOCKER_ACCOUNT:-YOUR_DOCKER_ACCOUNT_HERE}
 working_dir=${WORKING_DIR:-"/path/to/your/comfyui/code"}
 
-#########################
-# Kontext-Flux Builds
-#########################
-
-### Build kontext-models
-docker buildx build --target kontext-models \
-  --build-arg BUILDBOX_IMAGE="${docker_account}/buildbox:stable" \
-  -t "${docker_account}/ai-models:kontext-models-latest" "${working_dir}" \
-  -f "${working_dir}/docker/ai-models/kontext-models.Dockerfile"
-[[ "$?" -ne 0 ]] && echo "Error!" && return 11
-docker push "${docker_account}/ai-models:kontext-models-latest"
-[[ "$?" -ne 0 ]] && echo "Error!" && return 11
-
 ### Build comfyui:kontext-base
 docker buildx build --target comfyui \
   --build-arg BASE_IMAGE="ghcr.io/iamkalefreeman/comfyui-api:latest" \
