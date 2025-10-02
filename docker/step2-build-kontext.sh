e25d6bb24f8029f3af7e4d97f78becd07091a6fa
iamkalefreeman <70961907+iamkalefreeman@users.noreply.github.com>
2025-10-03 00:10:31 +0700

    Update step2-build-kontext.sh

--- docker/step2-build-kontext.sh
+++ docker/step2-build-kontext.sh
@@ -8,6 +8,7 @@ working_dir=${WORKING_DIR:-"/path/to/your/comfyui/code"}
 docker buildx build --target comfyui \
   --build-arg BASE_IMAGE="ghcr.io/iamkalefreeman/comfyui-api:latest" \
   --build-arg MODELS_IMAGE="${docker_account}/ai-models:kontext-models-latest" \
+  --build-arg COMFYUI_MODELS_IMAGE="${docker_account}/ai-models:comfyui-models-latest" \
   -t "${docker_account}/comfyui:kontext-base-latest" "${working_dir}" \
   -f "${working_dir}/docker/base/Dockerfile"
 [[ "$?" -ne 0 ]] && echo "Error!" && return 12
