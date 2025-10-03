5573e3d372de00c50620b2197c3d2aea66e19bec
iamkalefreeman <70961907+iamkalefreeman@users.noreply.github.com>
2025-10-04 00:22:12 +0700

    Update step1-build-qwen-models.sh

--- docker/step1-build-qwen-models.sh
+++ docker/step1-build-qwen-models.sh
@@ -14,6 +14,6 @@ docker buildx build --target qwen-models \
   -t "${docker_account}/ai-models:qwen-models-latest" "${working_dir}" \
   -f "${working_dir}/docker/ai-models/qwen-models.Dockerfile"
 [[ "$?" -ne 0 ]] && echo "Error!" && return 11
-# docker push "${docker_account}/ai-models:qwen-models-latest"
-# [[ "$?" -ne 0 ]] && echo "Error!" && return 11
+docker push "${docker_account}/ai-models:qwen-models-latest"
+[[ "$?" -ne 0 ]] && echo "Error!" && return 11
 
