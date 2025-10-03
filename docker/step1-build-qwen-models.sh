170f92bf8147cb0a3ce79c8c9d2cafbcf0732e75
iamkalefreeman <70961907+iamkalefreeman@users.noreply.github.com>
2025-10-03 16:27:19 +0700

    Update step1-build-qwen-models.sh

--- docker/step1-build-qwen-models.sh
+++ docker/step1-build-qwen-models.sh
@@ -8,14 +8,14 @@ working_dir=${WORKING_DIR:-"/path/to/your/comfyui/code"}
 # QWEN Builds
 #########################
 
-# ### Build qwen-models
-# docker buildx build --target qwen-models \
-#   --build-arg BUILDBOX_IMAGE="${docker_account}/buildbox:stable" \
-#   -t "${docker_account}/ai-models:qwen-models-latest" "${working_dir}" \
-#   -f "${working_dir}/docker/ai-models/qwen-models.Dockerfile"
+### Build qwen-models
+docker buildx build --target qwen-models \
+  --build-arg BUILDBOX_IMAGE="${docker_account}/buildbox:stable" \
+  -t "${docker_account}/ai-models:qwen-models-latest" "${working_dir}" \
+  -f "${working_dir}/docker/ai-models/qwen-models.Dockerfile"
+[[ "$?" -ne 0 ]] && echo "Error!" && return 11
+# docker push "${docker_account}/ai-models:qwen-models-latest"
 # [[ "$?" -ne 0 ]] && echo "Error!" && return 11
-# # docker push "${docker_account}/ai-models:qwen-models-latest"
-# # [[ "$?" -ne 0 ]] && echo "Error!" && return 11
 
 ### Build comfyui:qwen-models
 docker buildx build --target comfyui-with-models \
