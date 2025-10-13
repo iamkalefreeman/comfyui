51c3eccf433eabd0064bd179a4894d0596e1a12d
iamkalefreeman <70961907+iamkalefreeman@users.noreply.github.com>
2025-10-13 21:55:03 +0700

    Update rp_handler.py

--- docker/runpod/src/rp_handler.py
+++ docker/runpod/src/rp_handler.py
@@ -4,8 +4,9 @@ import os
 import json
 import logging
 import runpod
-import requests
-from requests.adapters import HTTPAdapter, Retry
+import aiohttp
+import asyncio
+from aiohttp import ClientTimeout
 
 # Environment variable handling
 try:
@@ -15,30 +16,29 @@ except ValueError:
 
 LOCAL_URL = os.environ.get("API_URL", "http://127.0.0.1:3000")
 
-cog_session = requests.Session()
-retries = Retry(total=10, backoff_factor=1.0, status_forcelist=[502, 503, 504])
-cog_session.mount('http://', HTTPAdapter(max_retries=retries))
+async_session = None
 
 # ---------------------------------------------------------------------------- #
 #                              Automatic Functions                             #
 # ---------------------------------------------------------------------------- #
-def wait_for_service(url, max_attempts=240):
+async def wait_for_service(url, max_attempts=240):
     '''
     Check if the service is ready to receive requests.
     '''
     attempts = 0
     while attempts < max_attempts:
         try:
-            health = requests.get(url, timeout=1)
-            if health.status_code == 200:
-                time.sleep(1)
-                return 200, None
-        except requests.exceptions.RequestException:
+            timeout_health = ClientTimeout(total=1)
+            async with async_session.get(url, timeout=timeout_health) as health:
+                if health.status == 200:
+                    await asyncio.sleep(1)
+                    return 200, None
+        except aiohttp.ClientError:
             print("Service not ready yet. Waiting for a second...")
         except Exception as err:
             print("Error: ", err)
 
-        time.sleep(1)
+        await asyncio.sleep(1)
         attempts += 1
         
     # Don't use "raise Exception()" before runpod.serverless.start() because that will cause Runpod worker to run indefinitely.
@@ -80,23 +80,29 @@ def validate_input(job_input):
     # Return validated data and no error
     return {"endpoint": endpoint, "body": body}, None
 
-def run_inference(endpoint, body):
+async def run_inference(endpoint, body):
     '''
     Run inference on a request.
     '''
     try:
-        response = cog_session.post(url=f'{LOCAL_URL}/{endpoint}', json=body, timeout=TIMEOUT)
-        if response.status_code != 200:
-            print(f"Request failed - reason: HTTP_{response.status_code} {response.text}")
-            raise ValueError(f"Request failed - reason: HTTP_{response.status_code} {response.text}")
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.JSONDecodeError:
+        async with async_session.post(url=f'{LOCAL_URL}/{endpoint}', json=body) as response:
+            if response.status == 200:
+                return await response.json()
+            else:
+                if response.status not in [502, 503, 504]:
+                    text = await response.text()
+                    raise ValueError(f"Request failed - reason: HTTP_{response.status} {text}")
+                else:
+                    text = await response.text()
+                    print(f"Request failed - reason: HTTP_{response.status} {text}")
+    except (aiohttp.ContentTypeError, json.JSONDecodeError):
         raise ValueError(f"Invalid JSON response from server")
-    except requests.exceptions.RequestException as e:
+    except aiohttp.ClientError as e:
+        raise ValueError(f"Request failed: {str(e)}")
+    except Exception as e:
         raise ValueError(f"Request failed: {str(e)}")
 
-def handler(job):
+async def handler(job):
     '''
     This is the handler function that will be called by the serverless.
     '''
@@ -108,7 +114,7 @@ def handler(job):
     # Extract validated data
     endpoint = validated_data["endpoint"]
     body = validated_data["body"]
-    response_data = run_inference(endpoint, body)
+    response_data = await run_inference(endpoint, body)
     return response_data
 
 def init_failed(job):
@@ -121,8 +127,14 @@ def adjust_concurrency(current_concurrency):
     return 3
     
 if __name__ == "__main__":
+    # Create the async session
+    connector = aiohttp.TCPConnector(ssl=False)
+    timeout = ClientTimeout(total=TIMEOUT)
+    global async_session
+    async_session = aiohttp.ClientSession(timeout=timeout, connector=connector)
+    
     # Don't use "raise Exception()" before runpod.serverless.start() because that will cause Runpod worker to run indefinitely.
-    http_code, error_message = wait_for_service(url=f'{LOCAL_URL}/health')
+    http_code, error_message = asyncio.run(wait_for_service(url=f'{LOCAL_URL}/health'))
     if error_message is None:
         print("API Service is ready. Starting RunPod serverless handler...")
         runpod.serverless.start({
@@ -131,4 +143,3 @@ if __name__ == "__main__":
         })
     else:
         runpod.serverless.start({"handler": init_failed})
-    
