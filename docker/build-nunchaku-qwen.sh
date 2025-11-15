8bce601880506dcb9b32c8c6d6b9d00738010ff4
iamkalefreeman <70961907+iamkalefreeman@users.noreply.github.com>
2025-11-15 09:01:20 +0700

    Create build-nunchaku-qwen.sh

---
+++ docker/build-nunchaku-qwen.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -x
+
+date_version=$(date +'%d-%m-%Y-TT-%H-%M')
+docker_account=${DOCKER_ACCOUNT:-YOUR_DOCKER_ACCOUNT_HERE}
+working_dir=${WORKING_DIR:-"/path/to/your/comfyui/code"}
+
+#########################
+# NUNCHAKU-QWEN Builds
+#########################
+
+### Build comfyui:nunchaku-qwen-base
+docker buildx build --target comfyui \
+  --build-arg BASE_IMAGE="ghcr.io/iamkalefreeman/comfyui-api:latest" \
+  --build-arg MODELS_IMAGE="${docker_account}/comfyui:nunchaku-qwen-models-latest" \
+  --build-arg COMFYUI_MODELS_IMAGE="${docker_account}/comfyui:comfyui-models-latest" \
+  -t "${docker_account}/comfyui:nunchaku-qwen-base-latest" "${working_dir}" \
+  -f "${working_dir}/docker/base/Dockerfile"
+[[ "$?" -ne 0 ]] && echo "Error!" && return 12
+docker push "${docker_account}/comfyui:nunchaku-qwen-base-latest"
+[[ "$?" -ne 0 ]] && echo "Error!" && return 12
+
+### Build comfyui:nunchaku-qwen-runpod
+docker buildx build --target runpod \
+  --build-arg BASE_IMAGE="${docker_account}/comfyui:nunchaku-qwen-base-latest" \
+  -t "${docker_account}/comfyui:nunchaku-qwen-runpod-${date_version}" \
+  -t "${docker_account}/comfyui:nunchaku-qwen-runpod-latest" "${working_dir}" \
+  -f "${working_dir}/docker/runpod/Dockerfile"
+[[ "$?" -ne 0 ]] && echo "Error!" && return 14
+docker push "${docker_account}/comfyui:nunchaku-qwen-runpod-${date_version}"
+# docker image rm "${docker_account}/comfyui:nunchaku-qwen-runpod-${date_version}"
+[[ "$?" -ne 0 ]] && echo "Error!" && return 14
+docker push "${docker_account}/comfyui:nunchaku-qwen-runpod-latest"
+# docker image rm "${docker_account}/comfyui:nunchaku-qwen-runpod-latest"
+[[ "$?" -ne 0 ]] && echo "Error!" && return 14
+
+# ### Build comfyui:nunchaku-qwen-full
+# docker buildx build --target full \
+#   --build-arg BASE_IMAGE="${docker_account}/comfyui:nunchaku-qwen-base-latest" \
+#   -t "${docker_account}/comfyui:nunchaku-qwen-full-latest" "${working_dir}" \
+#   -f "${working_dir}/docker/full/Dockerfile"
+# [[ "$?" -ne 0 ]] && echo "Error!" && return 15
+# docker push "${docker_account}/comfyui:nunchaku-qwen-full-latest"
+# [[ "$?" -ne 0 ]] && echo "Error!" && return 15
+
+return 0
